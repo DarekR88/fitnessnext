98c4377ffd96c1afc1e71f1c660cff6a4e6d13e0
Darek Radke <dradmclad@gmail.com>
2023-11-23 16:10:26 -0600

    navbar component finished

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,20 +12,31 @@ import FacebookIcon from "../../public/icons/facebook-svgrepo-com.svg";
 import InstaIcon from "../../public/icons/instagram-svgrepo-com.svg";
 import TiktokIcon from "../../public/icons/tiktok-logo-4512.svg";
 
-const [isOpen, setIsOpen] = useState(false);
-
-  
-const handleClick = () => {
-    setIsOpen(!isOpen);
-};
-
 export function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
+      <div
+        className={`absolute flex flex-col w-screen h-screen z-10 bg-blue-100 text-center pt-20 ${
+          isOpen ? "transition ease-in-out translate-y-0 duration-500" : "transition ease-in-out -translate-y-full duration-500"
+        }`}
+      >
+        <Link onClick={handleClick} className="text-xl" href="/">
+          Home
+        </Link>
+        <Link onClick={handleClick} className="text-xl" href="/about">
+          About
+        </Link>
+      </div>
       <header className="flex flex-col gap-4 pt-6 mb- justify-center items-center w-screen lg:flex-row">
         <button
           onClick={handleClick}
-          className="flex flex-col justify-center items-center lg:hidden"
+          className="flex flex-col justify-center items-center z-20 lg:hidden"
         >
           <span
             className={`bg-black block transition-all duration-300 ease-out 
@@ -46,8 +57,8 @@ export function NavBar() {
                     }`}
           ></span>
         </button>
-        <p className="font-Raleway text-2xl">Maximum Fitness</p>
-        <div className="flex gap-8 px-2 mb-2">
+        <p className="font-Raleway text-2xl z-0">Maximum Fitness</p>
+        <div className="flex gap-8 px-2 mb-2 z-0">
           <a
             href="mailto:darekradke2@gmail.com"
             target="_blank"
