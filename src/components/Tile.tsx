4c760f791c7dcdd674436e274b34ac33c3bc14d7
Darek Radke <dradmclad@gmail.com>
2023-11-25 01:34:42 -0600

    css for tile component

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -3,30 +3,29 @@
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 
-type IntroCardProps = {
+type TileProps = {
   tileImg: StaticImageData;
   title: string;
   blurb: string;
 };
 
-export function Tiles({ tileImg, title, blurb }: IntroCardProps) {
+export function Tile({ tileImg, title, blurb }: TileProps) {
   return (
     <>
-      <div className="absolute bg-black">
+      <div>
         <div>
           <Image
             src={tileImg}
             alt="weights"
             width={500}
-            className="opacity-50"
           />
         </div>
-        <div className="absolute text-white bottom-0 right-3">
-          <p className="text-7xl">{title}</p>
+        <div>
+          <p className="text-2xl">{title}</p>
         </div>
       </div>
-      <div className="mt-72">
-        <p className="mx-4">{blurb}</p>
+      <div>
+        <p className="mx-4 py-4">{blurb}</p>
       </div>
     </>
   );
