94c9eb13b0dd5378b3f20e4c076b273c069f741b
Darek Radke <dradmclad@gmail.com>
2023-11-26 01:40:57 -0600

    weight loss page

---
+++ src/app/weightloss/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+import { IntroCard } from "@/components/IntroCard";
+import { MoreInfo } from "@/components/MoreInfo"
+import introImg from "../../../public/images/weights.jpg";
+
+export default function WeightLoss() {
+  return (
+    <>
+      <IntroCard
+        introImg={introImg}
+        title="Weight Loss"
+        blurb="Weight loss can be an extremely difficult process. The personal 
+        trainers at Maximum Fitness are trained to be sensitive about your needs
+        and firm about keeping you accountable. Whether your goal is to lose 20 
+        pounds or 200, our personal trainers will have your back for every step 
+        of the way"
+      />
+      <MoreInfo />
+    </>
+  );
+}
