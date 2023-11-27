ebb01c42eefc995818ef27a579b34ed81df70728
Darek Radke <dradmclad@gmail.com>
2023-11-26 21:50:55 -0600

    css for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 export function Footer() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center w-screen lg:flex-row lg:px-40 lg:justify-between lg:h-36">
-        <p className="text-2xl">Maximum Fitness</p>
+      <div className="flex flex-col-reverse justify-center items-center w-screen border-t-2 border-black lg:flex-row lg:px-40 lg:justify-between lg:h-24">
         <p><span>&copy;</span> 2023 Darek Radke</p>
+        <p className="text-2xl">Maximum Fitness</p>
         <Link href={"/contact"} className="text-xl">Free Evaluations!</Link>
       </div>
     </>
