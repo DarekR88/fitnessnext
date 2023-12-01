44b846401063a5f8590335409415388bac3606f6
Darek Radke <dradmclad@gmail.com>
2023-11-30 19:42:15 -0600

    color variables added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        "bg-primary": "var(--bg-primary)"
+        "bg-primary": "var(--bg-primary)",
+        "bg-secondary": "var(--bg-secondary)",
+        "font-primary": "var(--font-primary)",
+        "font-secondary": "var(--font-secondary)",
+        "button-primary": "var(--button-primary)",
+        "button-secondary": "var(--button-secondary)",
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
