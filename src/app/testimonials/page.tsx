fd9915dc9bf6204508096b8a2256ea2e226db9a7
Darek Radke <dradmclad@gmail.com>
2023-12-02 17:59:04 -0600

    testimonials and footer css

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { IntroCard } from "@/components/IntroCard";
 import { FreeEval } from "@/components/FreeEval";
-import { MoreInfo } from "@/components/MoreInfo";
 import introImg from "../../../public/images/weights.jpg";
 import fatImg from "../../../public/images/jade-destiny-w6APf4aCmns-unsplash.jpg";
 import skinnyImg from "../../../public/images/vladimir-yelizarov-C43tCJffOh4-unsplash.jpg";
@@ -19,45 +18,46 @@ export default function Testimonials() {
         let our clients' results speak for themselves."
       />
       <FreeEval />
-      <div className="flex flex-col max-w-[1200px] m-auto lg:flex-row">
-        <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:rounded-l-md lg:h-[400px]">
-          <div className="w-1/2">
-            <Image src={fatImg} alt="fat" className="" />
+      <div className="w-screen lg:py-12 bg-gradient-to-b from-button-secondary to-gray-900">
+        <div className="flex flex-col max-w-[1200px] m-auto lg:flex-row">
+          <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:rounded-l-md lg:h-[400px]">
+            <div className="w-1/2">
+              <Image src={fatImg} alt="fat" className="" />
+            </div>
+            <div className="w-1/2">
+              <Image src={skinnyImg} alt="skinny" className="" />
+            </div>
           </div>
-          <div className="w-1/2">
-            <Image src={skinnyImg} alt="skinny" className="" />
+          <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-gradient-to-b from-button-secondary to-gray-900 text-font-secondary lg:rounded-r-md">
+            <p className="text-3xl">Jane Doe</p>
+            <p className="text-left text-xl leading-9">
+              "I lost 5 million pounds and became black. i would definitely
+              recommend Maximum Fitness for all your personal training needs. My
+              personal trainer was invaluable to my fitness journey and was
+              always on time."
+            </p>
           </div>
         </div>
-        <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-gradient-to-b from-button-secondary to-gray-900 text-font-secondary lg:rounded-r-md">
-          <p className="text-3xl">Jane Doe</p>
-          <p className="text-left text-xl leading-9">
-            "I lost 5 million pounds and became black. i would definitely
-            recommend Maximum Fitness for all your personal training needs. My
-            personal trainer was invaluable to my fitness journey and was always
-            on time."
-          </p>
+        <div className="flex flex-col pt-3 max-w-[1200px] m-auto lg:flex-row-reverse">
+          <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:rounded-r-md lg:h-[400px]">
+            <div className="w-1/2">
+              <Image src={fatImg} alt="fat" className="" />
+            </div>
+            <div className="w-1/2">
+              <Image src={skinnyImg} alt="skinny" className="" />
+            </div>
+          </div>
+          <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-gradient-to-b from-button-secondary to-gray-900 text-font-secondary lg:rounded-l-md">
+            <p className="text-3xl">John Joe</p>
+            <p className="text-left text-xl leading-9">
+              "I lost 5 million pounds and became black. i would definitely
+              recommend Maximum Fitness for all your personal training needs. My
+              personal trainer was invaluable to my fitness journey and was
+              always on time."
+            </p>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col pt-3 max-w-[1200px] m-auto lg:flex-row-reverse">
-        <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:rounded-r-md lg:h-[400px]">
-          <div className="w-1/2">
-            <Image src={fatImg} alt="fat" className="" />
-          </div>
-          <div className="w-1/2">
-            <Image src={skinnyImg} alt="skinny" className="" />
-          </div>
-        </div>
-        <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-gradient-to-b from-button-secondary to-gray-900 text-font-secondary lg:rounded-l-md">
-          <p className="text-3xl">John Joe</p>
-          <p className="text-left text-xl leading-9">
-            "I lost 5 million pounds and became black. i would definitely
-            recommend Maximum Fitness for all your personal training needs. My
-            personal trainer was invaluable to my fitness journey and was always
-            on time."
-          </p>
-        </div>
-      </div>
-      <MoreInfo />
     </>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function Footer() {
   return (
     <>
-      <div className="relative bottom-0 flex flex-col-reverse justify-center items-center w-screen py-6 mt-14 border-t-2 border-black lg:flex-row lg:px-32 lg:justify-between lg:h-24">
+      <div className="relative bottom-0 flex flex-col-reverse justify-center items-center w-screen py-6 bg-button-primary lg:flex-row lg:px-32 lg:justify-between lg:h-24">
         <p>
           <span>&copy;</span> 2023 Darek Radke
         </p>
