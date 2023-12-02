ba4114ca48161b0cabd150e10cbf77d09a692983
Darek Radke <dradmclad@gmail.com>
2023-12-02 16:36:10 -0600

    freeeval and home page css

--- src/components/FreeEval.tsx
+++ src/components/FreeEval.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 export function FreeEval() {
   return (
     <>
-      <div className="max-w-[1200px] m-auto h-[85px] text-center pt-[25px] lg:pt-[20px] mb-[12px] text-font-primary bg-gradient-to-b from-button-primary to-yellow-300 lg:rounded-md">
+      <div className="max-w-[775px] m-auto h-[100px] lg:h-[85px] text-center pt-[20px] lg:pt-[20px] mb-[12px] text-font-primary bg-gradient-to-b from-button-primary to-yellow-300 lg:rounded-md">
         <Link href={"/contact"} className="text-3xl font-BebasNeue lg:text-5xl tracking-widest">
-          Free Evaluation
+          Call or Text for a Free Evaluation
         </Link>
       </div>
     </>
