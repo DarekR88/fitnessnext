b7265db9c00b42b41fbcd09f317ee23d397851e3
Darek Radke <dradmclad@gmail.com>
2023-12-01 20:28:52 -0600

    home page css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <FreeEval />
       <div className="flex flex-wrap pb-3 w-full lg:max-w-[1200px] lg:justify-center lg:m-auto lg:justify-between">
         <div className="lg:w-[390px]">
-          <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-button-secondary lg:z-0 lg:rounded-md"></div>
+          <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-bg-secondary lg:z-0 lg:rounded-md"></div>
           <div className="lg:relative lg:-mt-[250px] lg:h-[250px] overflow-hidden rounded-md z-10 lg:transform transition lg:opacity-50 duration-500 lg:hover:opacity-100 lg:hover:scale-105">
             <Image
               src={WeightImg}
@@ -49,7 +49,7 @@ export default function Home() {
           </div>
         </div>
         <div className="lg:w-[390px]">
-          <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-button-primary lg:z-0 lg:rounded-md"></div>
+          <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-bg-secondary lg:z-0 lg:rounded-md"></div>
           <div className="lg:relative lg:-mt-[250px] lg:h-[250px] overflow-hidden rounded-md z-10 lg:transform transition lg:opacity-50 duration-500 lg:hover:opacity-100 lg:hover:scale-105">
             <Image
               src={WeightImg}
