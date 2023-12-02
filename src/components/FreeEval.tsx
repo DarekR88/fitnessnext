ad36f3a83a56e5254b4ae9fab96df7961934bde2
Darek Radke <dradmclad@gmail.com>
2023-12-01 18:17:42 -0600

    css for freeeval page

--- src/components/FreeEval.tsx
+++ src/components/FreeEval.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export function FreeEval() {
   return (
     <>
-      <div className="max-w-[1200px] m-auto h-[85px] text-center pt-[25px] lg:pt-[20px] mb-[12px] text-font-secondary bg-button-primary lg:rounded-md">
-        <Link href={"/contact"} className="text-3xl text-font-secondary font-BebasNeue lg:text-5xl tracking-widest">
+      <div className="max-w-[1200px] m-auto h-[85px] text-center pt-[25px] lg:pt-[20px] mb-[12px] text-font-primary bg-button-primary lg:rounded-md">
+        <Link href={"/contact"} className="text-3xl font-BebasNeue lg:text-5xl tracking-widest">
           Free Evaluation
         </Link>
       </div>
