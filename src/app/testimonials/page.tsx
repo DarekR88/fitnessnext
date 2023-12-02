46c1892ec958346a3792f50a13c3a84099bc142d
Darek Radke <dradmclad@gmail.com>
2023-12-01 20:02:00 -0600

    testimonials page css and moreinfo css

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { IntroCard } from "@/components/IntroCard";
 import { FreeEval } from "@/components/FreeEval";
+import { MoreInfo } from "@/components/MoreInfo";
 import introImg from "../../../public/images/weights.jpg";
 import fatImg from "../../../public/images/jade-destiny-w6APf4aCmns-unsplash.jpg";
 import skinnyImg from "../../../public/images/vladimir-yelizarov-C43tCJffOh4-unsplash.jpg";
@@ -37,6 +38,26 @@ export default function Testimonials() {
           </p>
         </div>
       </div>
+      <div className="flex flex-col pt-3 max-w-[1200px] m-auto lg:flex-row-reverse">
+        <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:rounded-r-md lg:h-[400px]">
+          <div className="w-1/2">
+            <Image src={fatImg} alt="fat" className="" />
+          </div>
+          <div className="w-1/2">
+            <Image src={skinnyImg} alt="skinny" className="" />
+          </div>
+        </div>
+        <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-button-secondary lg:rounded-l-md">
+          <p className="text-3xl">John Joe</p>
+          <p className="text-left text-xl leading-9">
+            "I lost 5 million pounds and became black. i would definitely
+            recommend Maximum Fitness for all your personal training needs. My
+            personal trainer was invaluable to my fitness journey and was always
+            on time."
+          </p>
+        </div>
+      </div>
+      <MoreInfo />
     </>
   );
 }
--- src/components/MoreInfo.tsx
+++ src/components/MoreInfo.tsx
@@ -11,8 +11,8 @@ import PhoneSvg from "../../public/icons/phone-svgrepo-com.svg";
 export function MoreInfo() {
   return (
     <>
-      <div className="text-center">
-        <p className="text-xl mx-4">
+      <div className="text-center mt-3 bg-button-primary max-w-[1200px] m-auto pt-3 lg:rounded-md">
+        <p className="text-xl px-3 font-BebasNeue tracking-widest lg:text-2xl">
           For more information call, text, or message to get a free evaluation.
         </p>
         <div className="flex justify-center py-6">
