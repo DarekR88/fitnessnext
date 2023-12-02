28be4aa15be74322ff1c97b96106e1f75757df03
Darek Radke <dradmclad@gmail.com>
2023-12-02 17:36:58 -0600

    home page and freeeval css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,91 +21,93 @@ export default function Home() {
           comes to reaching your goals."
       />
       <FreeEval />
-      <div className="flex flex-wrap pb-3 w-full lg:max-w-[1200px] lg:justify-center lg:m-auto lg:justify-between">
-        <div className="lg:w-[390px]">
-          <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-gradient-to-b from-gray-400 to-bg-secondary lg:z-0 lg:rounded-md"></div>
-          <div className="lg:relative lg:-mt-[250px] lg:h-[250px] overflow-hidden rounded-md z-10 lg:transform transition lg:opacity-50 duration-500 lg:hover:opacity-100">
-            <Link href={"/contact"} className="font-semibold">
-              <Image
-                src={WeightImg}
-                alt="weights"
-                width={500}
-                className="rounded-md"
-              />
-            </Link>
+      <div className="bg-gradient-to-b from-button-secondary to-[#073635] text-font-secondary lg:py-12">
+        <div className="flex flex-wrap pb-3 w-full lg:max-w-[1200px] lg:justify-center lg:m-auto lg:justify-between">
+          <div className="lg:w-[390px]">
+            <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-gradient-to-b from-gray-400 to-bg-secondary lg:z-0 lg:rounded-md"></div>
+            <div className="lg:relative lg:-mt-[250px] lg:h-[250px] overflow-hidden rounded-md z-10 lg:transform transition lg:opacity-50 duration-500 lg:hover:opacity-100">
+              <Link href={"/contact"} className="font-semibold">
+                <Image
+                  src={WeightImg}
+                  alt="weights"
+                  width={500}
+                  className="rounded-md"
+                />
+              </Link>
+            </div>
+            <div className="text-2xl ml-3 py-2">
+              <Link href={"/contact"} className="font-semibold">
+                Free Evaluations
+              </Link>
+            </div>
+            <p className="mx-4">
+              Evaluations are completely free and can be done in person at the
+              location of your choosing, through video chat, or by phone. At
+              Maximum fitness we understand the first step can be the hardest.
+            </p>
+            <div className="w-full text-center pt-3 pb-8 text-2xl lg:pb-0">
+              <Link href={"/contact"} className="font-semibold">
+                More Info
+              </Link>
+            </div>
           </div>
-          <div className="text-2xl ml-3 py-2">
-            <Link href={"/contact"} className="font-semibold">
-              Free Evaluations
-            </Link>
+          <div className="lg:w-[390px]">
+            <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-gradient-to-b from-gray-400 to-bg-secondary lg:z-0 lg:rounded-md"></div>
+            <div className="lg:relative lg:-mt-[250px] lg:h-[250px] overflow-hidden rounded-md z-10 lg:transform transition lg:opacity-50 duration-500 lg:hover:opacity-100">
+              <Link href={"/contact"} className="font-semibold">
+                <Image
+                  src={WeightImg}
+                  alt="weights"
+                  width={500}
+                  className="rounded-md"
+                />
+              </Link>
+            </div>
+            <div className="text-2xl ml-3 py-2">
+              <Link href={"/pricing"} className="font-semibold">
+                Affordable Pricing
+              </Link>
+            </div>
+            <p className="mx-4">
+              At Maximum Fitness we believe personal training should be
+              affordable. At $65/<span className="text-sm">hr.</span> for in
+              person sessions and $40/<span className="text-sm">hr.</span> for
+              video call sessions we offer the most competitive prices in the
+              industry.
+            </p>
+            <div className="w-full text-center pt-3 pb-8 text-2xl lg:pb-0">
+              <Link href={"/pricing"} className="font-semibold">
+                More Info
+              </Link>
+            </div>
           </div>
-          <p className="mx-4">
-            Evaluations are completely free and can be done in person at the
-            location of your choosing, through video chat, or by phone. At
-            Maximum fitness we understand the first step can be the hardest.
-          </p>
-          <div className="w-full text-center pt-3 pb-8 text-2xl lg:pb-0">
-            <Link href={"/contact"} className="font-semibold">
-              More Info
-            </Link>
-          </div>
-        </div>
-        <div className="lg:w-[390px]">
-          <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-gradient-to-b from-gray-400 to-bg-secondary lg:z-0 lg:rounded-md"></div>
-          <div className="lg:relative lg:-mt-[250px] lg:h-[250px] overflow-hidden rounded-md z-10 lg:transform transition lg:opacity-50 duration-500 lg:hover:opacity-100">
-          <Link href={"/contact"} className="font-semibold">
-              <Image
-                src={WeightImg}
-                alt="weights"
-                width={500}
-                className="rounded-md"
-              />
-            </Link>
-          </div>
-          <div className="text-2xl ml-3 py-2">
-            <Link href={"/pricing"} className="font-semibold">
-              Affordable Pricing
-            </Link>
-          </div>
-          <p className="mx-4">
-            At Maximum Fitness we believe personal training should be
-            affordable. At $65/<span className="text-sm">hr.</span> for in
-            person sessions and $40/<span className="text-sm">hr.</span> for
-            video call sessions we offer the most competitive prices in the
-            industry.
-          </p>
-          <div className="w-full text-center pt-3 pb-8 text-2xl lg:pb-0">
-            <Link href={"/pricing"} className="font-semibold">
-              More Info
-            </Link>
-          </div>
-        </div>
-        <div className="lg:w-[390px]">
-          <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-gradient-to-b from-gray-400 to-bg-secondary lg:z-0 lg:rounded-md"></div>
-          <div className="lg:relative lg:-mt-[250px] lg:h-[250px] overflow-hidden rounded-md z-10 lg:transform transition lg:opacity-50 duration-500 lg:hover:opacity-100">
-          <Link href={"/contact"} className="font-semibold">
-              <Image
-                src={WeightImg}
-                alt="weights"
-                width={500}
-                className="rounded-md"
-              />
-            </Link>
-          </div>
-          <div className="text-2xl ml-3 py-2 z-10">
-            <Link href={"/programs"} className="font-semibold">
-              Cutting Edge Programs
-            </Link>
-          </div>
-          <p className="mx-4">
-            Maximum Fitness training programs are based on the most current
-            studies and kinesiology. Our competitive pricing does not effect our
-            ability to provide the highest quality training.
-          </p>
-          <div className="w-full text-center pt-3 pb-6 text-2xl lg:pb-0">
-            <Link href={"/programs"} className="font-semibold">
-              More Info
-            </Link>
+          <div className="lg:w-[390px]">
+            <div className="hidden lg:block lg:relative lg:h-[250px] lg:w-[390px] lg:bg-gradient-to-b from-gray-400 to-bg-secondary lg:z-0 lg:rounded-md"></div>
+            <div className="lg:relative lg:-mt-[250px] lg:h-[250px] overflow-hidden rounded-md z-10 lg:transform transition lg:opacity-50 duration-500 lg:hover:opacity-100">
+              <Link href={"/contact"} className="font-semibold">
+                <Image
+                  src={WeightImg}
+                  alt="weights"
+                  width={500}
+                  className="rounded-md"
+                />
+              </Link>
+            </div>
+            <div className="text-2xl ml-3 py-2 z-10">
+              <Link href={"/programs"} className="font-semibold">
+                Cutting Edge Programs
+              </Link>
+            </div>
+            <p className="mx-4">
+              Maximum Fitness training programs are based on the most current
+              studies and kinesiology. Our competitive pricing does not effect
+              our ability to provide the highest quality training.
+            </p>
+            <div className="w-full text-center pt-3 pb-6 text-2xl lg:pb-0">
+              <Link href={"/programs"} className="font-semibold">
+                More Info
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/components/FreeEval.tsx
+++ src/components/FreeEval.tsx
@@ -5,10 +5,15 @@ import Link from "next/link";
 export function FreeEval() {
   return (
     <>
-      <div className="max-w-[775px] m-auto h-[100px] lg:h-[85px] text-center pt-[20px] lg:pt-[20px] mb-[12px] text-font-primary bg-gradient-to-b from-button-primary to-yellow-400 lg:rounded-md">
-        <Link href={"/contact"} className="text-3xl font-BebasNeue lg:text-5xl tracking-widest">
-          Call or Text for a Free Evaluation
-        </Link>
+      <div className="flex bg-gradient-to-t from-button-primary to-yellow-400 h-[120px] w-screen justify-center items-center">
+        <div className="lg:w-[775px] m-auto h-[100px] lg:h-[85px] text-center pt-[20px] lg:pt-[20px] text-font-primary bg-gradient-to-b from-button-primary to-yellow-400 lg:rounded-md">
+          <Link
+            href={"/contact"}
+            className="text-3xl font-BebasNeue lg:text-5xl tracking-widest"
+          >
+            Call or Text for a Free Evaluation
+          </Link>
+        </div>
       </div>
     </>
   );
