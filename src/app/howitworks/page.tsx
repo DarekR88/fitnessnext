d52f366d9b55229f53be781eea6ebe4049da8e79
Darek Radke <dradmclad@gmail.com>
2023-12-03 15:39:23 -0600

    howitworks page copy updated and links updated

--- src/app/howitworks/page.tsx
+++ src/app/howitworks/page.tsx
@@ -7,7 +7,6 @@ import { IntroCard } from "@/components/IntroCard";
 import { FreeEval } from "@/components/FreeEval";
 import WeightImg from "../../../public/images/weights.jpg";
 
-
 export default function HowItWorks() {
   return (
     <>
@@ -16,7 +15,7 @@ export default function HowItWorks() {
         title="How it Works"
         blurb="Initial evaluation and first workout are free! Start your fitness 
         journey by scheduling a free initial evaluation and first session. 
-        One of our skilled and professional personal trainers will come to your 
+        One of our skilled and professional personal trainers will start a virtual meeting, or come to your 
         home at the scheduled time to perform a free evaluation and guide you through your 
         free first workout(30 minutes)."
       />
@@ -38,13 +37,12 @@ export default function HowItWorks() {
             <div className="bg-gradient-to-b from-button-secondary to-[#073635] py-4">
               <div className="text-2xl ml-3 pb-2">
                 <Link href={"/contact"} className="font-semibold">
-                  Free Evaluations
+                  Free Evaluation and First Session
                 </Link>
               </div>
               <p className="mx-4">
-                Evaluations are completely free and can be done in person at the
-                location of your choosing, through video chat, or by phone. At
-                Maximum fitness we understand the first step can be the hardest.
+                Give personal training a try, no credit card required. Schedule
+                a free video call or in home personal training session now.
               </p>
               <div className="w-full text-center pt-3 pb-8 text-2xl lg:pb-0">
                 <Link href={"/contact"} className="font-semibold">
@@ -67,19 +65,17 @@ export default function HowItWorks() {
             </div>
             <div className="bg-gradient-to-b from-button-secondary to-[#073635] py-4">
               <div className="text-2xl ml-3 pb-2">
-                <Link href={"/pricing"} className="font-semibold">
-                  Affordable Pricing
+                <Link href={"/contact"} className="font-semibold">
+                  In Home Personal Training
                 </Link>
               </div>
               <p className="mx-4">
-                At Maximum Fitness we believe personal training should be
-                affordable. At $65/<span className="text-sm">hr.</span> for in
-                person sessions and $40/<span className="text-sm">hr.</span> for
-                video call sessions we offer the most competitive prices in the
-                industry.
+                We'll bring everything you need to reach your fitness goals.
+                Forget the traffic and crowded gym, get in shape in the comfort
+                of your own home.
               </p>
               <div className="w-full text-center pt-3 pb-8 text-2xl lg:pb-0">
-                <Link href={"/pricing"} className="font-semibold">
+                <Link href={"/contact"} className="font-semibold">
                   More Info
                 </Link>
               </div>
@@ -99,17 +95,17 @@ export default function HowItWorks() {
             </div>
             <div className="bg-gradient-to-b from-button-secondary to-[#073635] py-4">
               <div className="text-2xl ml-3 pb-2 z-10">
-                <Link href={"/programs"} className="font-semibold">
-                  Cutting Edge Programs
+                <Link href={"/contact"} className="font-semibold">
+                  Virtual Personal Training
                 </Link>
               </div>
               <p className="mx-4">
-                Maximum Fitness training programs are based on the most current
-                studies and kinesiology. Our competitive pricing does not effect
-                our ability to provide the highest quality training.
+                Meet your personal trainer through video call. Receive all the
+                benefits of personal training while staying contact free. We'll
+                keep you accountable and motivated.
               </p>
               <div className="w-full text-center pt-3 pb-6 text-2xl lg:pb-0">
-                <Link href={"/programs"} className="font-semibold">
+                <Link href={"/contact"} className="font-semibold">
                   More Info
                 </Link>
               </div>
