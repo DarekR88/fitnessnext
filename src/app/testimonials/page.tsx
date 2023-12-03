15f32808c5aeda7158a72499b6b07c91837ae914
Darek Radke <dradmclad@gmail.com>
2023-12-02 18:57:56 -0600

    testimonials page css

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -18,9 +18,9 @@ export default function Testimonials() {
         let our clients' results speak for themselves."
       />
       <FreeEval />
-      <div className="w-screen lg:py-12 bg-gradient-to-b from-button-secondary to-gray-900">
+      <div className="w-screen lg:py-12 bg-gradient-to-b from-button-secondary to-[#073635]">
         <div className="flex flex-col max-w-[1200px] m-auto lg:flex-row">
-          <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:rounded-l-md lg:h-[400px]">
+          <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:h-[400px]">
             <div className="w-1/2">
               <Image src={fatImg} alt="fat" className="" />
             </div>
@@ -28,7 +28,7 @@ export default function Testimonials() {
               <Image src={skinnyImg} alt="skinny" className="" />
             </div>
           </div>
-          <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-gradient-to-b from-button-secondary to-gray-900 text-font-secondary lg:rounded-r-md">
+          <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-gradient-to-b from-button-secondary to-[#073635] text-font-secondary">
             <p className="text-3xl">Jane Doe</p>
             <p className="text-left text-xl leading-9">
               "I lost 5 million pounds and became black. i would definitely
@@ -39,7 +39,7 @@ export default function Testimonials() {
           </div>
         </div>
         <div className="flex flex-col pt-3 max-w-[1200px] m-auto lg:flex-row-reverse">
-          <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:rounded-r-md lg:h-[400px]">
+          <div className="flex max-w-full h-[250px] overflow-hidden lg:w-1/2 lg:h-[400px]">
             <div className="w-1/2">
               <Image src={fatImg} alt="fat" className="" />
             </div>
@@ -47,7 +47,7 @@ export default function Testimonials() {
               <Image src={skinnyImg} alt="skinny" className="" />
             </div>
           </div>
-          <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-gradient-to-b from-button-secondary to-gray-900 text-font-secondary lg:rounded-l-md">
+          <div className="lg:w-1/2 text-center px-3 py-3 lg:px-4 lg:pt-14 bg-gradient-to-b from-button-secondary to-[#073635] text-font-secondary">
             <p className="text-3xl">John Joe</p>
             <p className="text-left text-xl leading-9">
               "I lost 5 million pounds and became black. i would definitely
