d45689f80f82abaeecfe909a933b5facc4738c0b
Darek Radke <dradmclad@gmail.com>
2023-12-03 13:38:19 -0600

    css for introcard component

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -22,7 +22,7 @@ export function IntroCard({ introImg, title, blurb }: IntroCardProps) {
           />
         </div>
         <div className="md:w-1/4 lg:pt-[20px] lg:w-[600px] text-font-secondary lg:rounded-r-md">
-          <div className="text-center lg:text-left ml-4">
+          <div className="text-left ml-4">
             <p className="text-3xl py-2 font-semibold text-font-secondary lg:text-5xl">{title}</p>
           </div>
           <p className="mx-4 pb-4">{blurb}</p>
